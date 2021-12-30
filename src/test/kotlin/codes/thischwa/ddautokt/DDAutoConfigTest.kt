56b0c8c9a7e714793c3c21f7042782d82c679e37
Thilo Schwarz <osp@thischwa.codes>
2021-12-30 16:48:23 +0100

    added  tests

--- src/test/kotlin/codes/thischwa/ddautokt/DDAutoConfigTest.kt
+++ src/test/kotlin/codes/thischwa/ddautokt/DDAutoConfigTest.kt
@@ -10,12 +10,22 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @SpringBootTest(classes = [DDAutoConfig::class] )
 @ExtendWith(SpringExtension::class)
-class DDAutoConfigTest  {
+class DDAutoConfigTest {
     @Autowired
-    lateinit var config : DDAutoConfig
+    lateinit var config: DDAutoConfig
 
     @Test
     fun testCountZones() {
         assertEquals(2, config.zones.size)
+    }
+
+    @Test
+    fun testZoneDetails() {
+        val zone: DDAutoConfig.Zone = config.zones[0]
+        assertEquals("dynhost0.info", zone.name)
+        assertEquals("ns0.domain.info", zone.ns)
+
+        assertEquals("my0:1234567890abcdef", zone.hosts[0])
+        assertEquals("test1:1234567890abcdx", zone.hosts[1])
     }
 }
