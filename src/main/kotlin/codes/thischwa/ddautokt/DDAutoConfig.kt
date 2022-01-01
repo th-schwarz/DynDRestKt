161d8b60debd21d4c12ae89090b8901ff8835700
Thilo Schwarz <osp@thischwa.codes>
2022-01-01 17:50:14 +0100

    add tests

--- src/main/kotlin/codes/thischwa/ddautokt/DDAutoConfig.kt
+++ src/main/kotlin/codes/thischwa/ddautokt/DDAutoConfig.kt
@@ -1,5 +1,6 @@
 package codes.thischwa.ddautokt
 
+import mu.KotlinLogging
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
@@ -8,13 +9,16 @@ import org.springframework.context.annotation.Configuration
 @EnableConfigurationProperties
 @ConfigurationProperties(prefix = "ddauto")
 class DDAutoConfig {
+
     lateinit var zones: List<Zone>
 
+    private val log = KotlinLogging.logger {}
+
     // <zone, ns>
-    private var zoneData : Map<String, String> = mutableMapOf()
+    private var zoneData = mutableMapOf<String, String>()
 
     // <fqdn, apitoken>
-    private var apitokenData : Map<String, String> = mutableMapOf()
+    private var apitokenData  = mutableMapOf<String, String>()
 
     fun configuredHosts() : Set<String> = apitokenData.keys
 
@@ -25,15 +29,47 @@ class DDAutoConfig {
     @Throws(IllegalArgumentException::class)
     fun apiToken(host : String) : String {
         if (!hostExists(host))
-            throw IllegalArgumentException("Host isn't configured: " + host)
+            throw IllegalArgumentException("Host isn't configured: $host")
         return apitokenData[host]!!
     }
 
     @Throws(IllegalArgumentException::class)
     fun primaryNameServer(zone : String) : String {
         if(!zoneData.containsKey(zone))
-            throw IllegalArgumentException("Zone isn't configured: " + zone)
+            throw IllegalArgumentException("Zone isn't configured: $zone")
         return zoneData[zone]!!
+    }
+
+    fun eadAndValidate() {
+        read()
+        validate()
+    }
+
+    @Throws(IllegalArgumentException::class)
+    fun validate() {
+        if (zoneData.isEmpty() || apitokenData.isEmpty())
+            throw IllegalArgumentException("Zone or host data is empty.")
+        log.info { "*** Configured hosts:" }
+        for (host in configuredHosts())
+            log.info { "  - $host" }
+    }
+
+    @Throws(IllegalArgumentException::class)
+    fun read() {
+        for (zone in zones) {
+            zoneData[zone.name]= zone.ns
+            var hostData = zone.hosts
+            if (hostData.isEmpty())
+                throw IllegalArgumentException("Missing host data for $zone")
+            for (hostToken in hostData) {
+                val partsAry = hostToken.split(":")
+                if (partsAry.size != 2)
+                    throw IllegalArgumentException("The host data must be in the following format: [sld]:[apitoken], but it was $hostToken")
+                // build the fqdn hostname
+                val host = partsAry[0] + "." + zone.name
+                apitokenData[host] = partsAry[1]
+            }
+        }
     }
 
     class Zone {
--- src/test/kotlin/codes/thischwa/ddautokt/DDAutoConfigTest.kt
+++ src/test/kotlin/codes/thischwa/ddautokt/DDAutoConfigTest.kt
@@ -1,22 +1,32 @@
 package codes.thischwa.ddautokt
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(classes = [DDAutoConfig::class] )
 @ExtendWith(SpringExtension::class)
 class DDAutoConfigTest {
     @Autowired
     lateinit var config: DDAutoConfig
 
+    val configuredEntries : Int = 2
+
+    @BeforeEach
+    fun setUp() {
+        config.read()
+    }
+
     @Test
     fun testCountZones() {
-        assertEquals(2, config.zones.size)
+        assertEquals(configuredEntries, config.zones.size)
     }
 
     @Test
@@ -27,5 +37,17 @@ class DDAutoConfigTest {
 
         assertEquals("my0:1234567890abcdef", zone.hosts[0])
         assertEquals("test1:1234567890abcdx", zone.hosts[1])
+    }
+
+    @Test
+    fun testApiToken() {
+        assertEquals("1234567890abcdef", config.apiToken("my0.dynhost0.info"))
+        assertThrows(IllegalArgumentException::class.java) { config.apiToken("unknown.host.info") }
+    }
+
+    @Test
+    fun testPrimaryNameServer() {
+        assertEquals("ns1.domain.info", config.primaryNameServer("dynhost1.info"))
+        assertThrows(IllegalArgumentException::class.java) { config.primaryNameServer("unknown-host.info") }
     }
 }
