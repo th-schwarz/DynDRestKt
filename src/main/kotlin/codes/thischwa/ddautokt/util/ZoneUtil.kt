b4a1e40fa5c053b1c0eeb513bab75280121f8ea4
Thilo Schwarz <osp@thischwa.codes>
2022-01-02 22:16:56 +0100

    wip

--- src/main/kotlin/codes/thischwa/ddautokt/util/ZoneUtil.kt
+++ src/main/kotlin/codes/thischwa/ddautokt/util/ZoneUtil.kt
@@ -1,21 +1,24 @@
 package codes.thischwa.ddautokt.util
 
+import org.domainrobot.sdk.models.generated.ResourceRecord
+import org.domainrobot.sdk.models.generated.Zone
 import java.net.Inet6Address
 import java.net.InetAddress
 import java.net.UnknownHostException
+import java.util.*
 
 
 class ZoneUtil {
 
-    private val DEFAULT_TLD: Long = 60
-
     companion object {
 
-        val RR_A = "A"
+        private val DEFAULT_TLD: Long = 60
 
-        val RR_AAAA = "AAAA"
+        var RR_A = "A"
 
-        fun deriveZone(host: String): String? {
+        var RR_AAAA = "AAAA"
+
+        fun deriveZone(host: String): String {
             val cnt = host.chars().filter { ch: Int -> ch == '.'.code }.count()
             require(cnt >= 2) { "'host' must be a sub domain." }
             return host.substring(host.indexOf(".") + 1)
@@ -40,6 +43,51 @@ class ZoneUtil {
 
         fun isValidateIP(ipStr: String): Boolean {
             return isIPv4(ipStr) || isIPv6(ipStr)
+        }
+
+        fun addOrUpdateIPv4(zone: Zone, sld: String, ip: String) {
+            addOrUpdateIP(zone, sld, ip, RR_A)
+        }
+
+        fun addOrUpdateIPv6(zone: Zone, sld: String, ip: String) {
+            addOrUpdateIP(zone, sld, ip, RR_AAAA)
+        }
+
+        private fun addOrUpdateIP(zone: Zone, sld: String, ip: String, type: String) {
+            val rr : ResourceRecord? = searchResourceRecord(zone, sld, type)
+            if (rr != null) {
+                rr.value = ip
+                rr.ttl = DEFAULT_TLD
+            } else {
+                val rrSld = ResourceRecord()
+                rrSld.name = sld
+                rrSld.value = ip
+                rrSld.type = type
+                rrSld.ttl = DEFAULT_TLD
+                zone.resourceRecords.add(rrSld)
+            }
+        }
+
+        fun removeIPv4(zone: Zone, sld: String) {
+            removeIP(zone, sld, RR_A)
+        }
+
+        fun removeIPv6(zone: Zone, sld: String) {
+            removeIP(zone, sld, RR_AAAA)
+        }
+
+        private fun removeIP(zone: Zone, sld: String, type: String) {
+            val rr: ResourceRecord? = searchResourceRecord(zone, sld, type)
+            if (rr != null) {
+                zone.resourceRecords.remove(rr)
+            }
+        }
+
+        fun searchResourceRecord(zone: Zone, name: String, type: String): ResourceRecord? {
+            val rrO: Optional<ResourceRecord> = zone.resourceRecords.stream()
+                .filter { rr: org.domainrobot.sdk.models.generated.ResourceRecord -> rr.type == type && rr.name == name }
+                .findFirst()
+            return if (rrO.isPresent) rrO.get() else null
         }
     }
 }
--- src/test/kotlin/codes/thischwa/ddautokt/util/ZoneUtilTest.kt
+++ src/test/kotlin/codes/thischwa/ddautokt/util/ZoneUtilTest.kt
@@ -1,11 +1,29 @@
 package codes.thischwa.ddautokt.util
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.domainrobot.sdk.client.JsonUtils
+import org.domainrobot.sdk.models.generated.JsonResponseDataZone
+import org.domainrobot.sdk.models.generated.Zone
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import java.io.InputStream
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ZoneUtilTest {
+
+    private lateinit var zone: Zone
+
+    private val rrCount : Int = 5
+
+    @BeforeEach
+    @Throws(Exception::class)
+    fun setUp() {
+        val content : String = ZoneUtilTest::class.java.getResource("/zone-info.json").readText()
+        val response = JsonUtils.deserialize(content.toByteArray(),
+            JsonResponseDataZone::class.java)
+        zone = response.data[0]
+    }
 
     @Test
     fun testValidIP() {
@@ -22,5 +40,71 @@ class ZoneUtilTest {
         assertFalse(ZoneUtil.isIPv6("217.229.139.240"))
     }
 
+    @Test
+    fun testDeriveZone() {
+        assertEquals("example.com", ZoneUtil.deriveZone("sub.example.com"))
+    }
+
+    @Test
+    fun testUpdateIPv4() {
+        assertEquals(rrCount, zone.resourceRecords.size)
+        ZoneUtil.addOrUpdateIPv4(zone, "sub", "128.0.0.1")
+        assertEquals(rrCount, zone.resourceRecords.size)
+        val rr = ZoneUtil.searchResourceRecord(zone, "sub", ZoneUtil.RR_A)
+        assertNotNull(rr)
+        assertEquals("128.0.0.1", rr!!.value)
+    }
+
+    @Test
+    fun testUpdateIPv6() {
+        assertEquals(rrCount, zone.resourceRecords.size)
+        ZoneUtil.addOrUpdateIPv6(zone, "sub", "2a03:4000:41:32::20")
+        assertEquals(rrCount, zone.resourceRecords.size)
+        val rr = ZoneUtil.searchResourceRecord(zone, "sub", ZoneUtil.RR_AAAA)
+        assertNotNull(rr)
+        assertEquals("2a03:4000:41:32::20", rr!!.value)
+    }
+
+    @Test
+    fun testAddIPv4() {
+        assertEquals(rrCount, zone.resourceRecords.size)
+        ZoneUtil.addOrUpdateIPv4(zone, "sub1", "128.0.0.1")
+        assertEquals(rrCount + 1, zone.resourceRecords.size)
+        val rr = ZoneUtil.searchResourceRecord(zone, "sub1", ZoneUtil.RR_A)
+        assertNotNull(rr)
+        assertEquals("128.0.0.1", rr!!.value)
+    }
+
+    @Test
+    fun testAddIPv6() {
+        assertEquals(rrCount, zone.resourceRecords.size)
+        ZoneUtil.addOrUpdateIPv6(zone, "sub1", "2a03:4000:41:32::20")
+        assertEquals(rrCount + 1, zone.resourceRecords.size)
+        val rr = ZoneUtil.searchResourceRecord(zone, "sub1", ZoneUtil.RR_AAAA)
+        assertNotNull(rr)
+        assertEquals("2a03:4000:41:32::20", rr!!.value)
+    }
+
+    @Test
+    fun testRemoveIPv4() {
+        assertEquals(rrCount, zone.resourceRecords.size)
+        ZoneUtil.addOrUpdateIPv4(zone, "sub2", "128.0.0.2")
+        assertEquals(rrCount + 1, zone.resourceRecords.size)
+        ZoneUtil.removeIPv4(zone, "sub2")
+        assertEquals(rrCount, zone.resourceRecords.size)
+        ZoneUtil.removeIPv4(zone, "unknownsub")
+        assertEquals(rrCount, zone.resourceRecords.size)
+    }
+
+    @Test
+    fun testRemoveIPv6() {
+        assertEquals(rrCount, zone.resourceRecords.size)
+        ZoneUtil.addOrUpdateIPv6(zone, "sub2", "2a03:4000:41:32::20")
+        assertEquals(rrCount + 1, zone.resourceRecords.size)
+        ZoneUtil.removeIPv6(zone, "sub2")
+        assertEquals(rrCount, zone.resourceRecords.size)
+        ZoneUtil.removeIPv6(zone, "unknownsub")
+        assertEquals(rrCount, zone.resourceRecords.size)
+    }
 
 }
